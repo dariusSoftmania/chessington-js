08d804ad6b5d39ea6d55ad96b8cf95c386daec0a
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-10 15:23:25 +0300

    refactor pawn move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -12,18 +12,24 @@ export default class Pawn extends Piece {
         let moves: Square[] = [];
         const square: Square = board.findPiece(this);
         if(this.player === Player.WHITE) {
-            if(board.getPiece(new Square(square.row + 1, square.col)) !== undefined)
+            let nextRow: number = square.row + 1;
+            const col: number = square.col;
+            if(board.getPiece(new Square(nextRow, col)) !== undefined)
                 return moves;
-            moves.push(new Square(square.row + 1, square.col));
-            if(square.row === 1 && board.getPiece(new Square(square.row + 2, square.col)) === undefined)
-                moves.push(new Square(square.row + 2, square.col))
+            moves.push(new Square(nextRow, col));
+            nextRow++;
+            if(square.row === 1 && board.getPiece(new Square(nextRow, col)) === undefined)
+                moves.push(new Square(nextRow, col));
         }
         else {
-            if(board.getPiece(new Square(square.row - 1, square.col)) !== undefined)
+            let nextRow: number = square.row - 1;
+            const col: number = square.col;
+            if(board.getPiece(new Square(nextRow, col)) !== undefined)
                 return moves;
-            moves.push(new Square(square.row - 1, square.col));
-            if(square.row === 6 && board.getPiece(new Square(square.row - 2, square.col)) === undefined)
-                moves.push(new Square(square.row - 2, square.col));
+            moves.push(new Square(nextRow, col));
+            nextRow--;
+            if(square.row === 6 && board.getPiece(new Square(nextRow, col)) === undefined)
+                moves.push(new Square(nextRow, col));
         }
         return moves;
     }
