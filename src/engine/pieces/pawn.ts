d3710461bdceeab5ca45451053629bcac9104e3d
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-10 12:27:06 +0300

    pawn can move forward

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,12 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+        let square: Square = board.findPiece(this);
+        if(this.player === 0)
+            moves.push(new Square(square.row + 1, square.col))
+        else
+            moves.push(new Square(square.row - 1, square.col))
+        return moves;
     }
 }
