938ef334a3539178a5bd35fedea759b4d9c8e2cd
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-10 13:41:41 +0300

    bishop can move diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,13 +1,26 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        let moves: Square[] = [];
+        const square: Square = board.findPiece(this);
+        for(let index: number = 1; index <= 7; ++index) {
+            if (board.checkBounds(square.row - index, square.col - index))
+                moves.push(new Square(square.row - index, square.col - index));
+            if (board.checkBounds(square.row - index, square.col + index))
+                moves.push(new Square(square.row - index, square.col + index));
+            if (board.checkBounds(square.row + index, square.col - index))
+                moves.push(new Square(square.row + index, square.col - index));
+            if (board.checkBounds(square.row + index, square.col + index))
+                moves.push(new Square(square.row + index, square.col + index));
+        }
+        return moves;
     }
 }
