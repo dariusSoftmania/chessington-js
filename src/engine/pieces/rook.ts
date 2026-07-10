a2f40af08c07b71cabd3f55a8c18fd9041c67fc9
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-10 13:15:23 +0300

    rook can move horizontal and vertical

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,14 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = [];
+        let square: Square = board.findPiece(this);
+        for(let col = 0; col <= 7; ++col)
+            if (col !== square.col)
+                moves.push(new Square(square.row, col));
+        for(let row = 0; row <= 7; ++row)
+            if (row !== square.row)
+                moves.push(new Square(row, square.col));
+        return moves;
     }
 }
