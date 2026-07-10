cfa8f2043ac3aa72124025d27260963d9d27cc5d
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-10 15:11:40 +0300

    pawn cannot move if destination has another piece

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -12,13 +12,17 @@ export default class Pawn extends Piece {
         let moves: Square[] = [];
         const square: Square = board.findPiece(this);
         if(this.player === Player.WHITE) {
+            if(board.getPiece(new Square(square.row + 1, square.col)) !== undefined)
+                return moves;
             moves.push(new Square(square.row + 1, square.col));
-            if(square.row === 1)
+            if(square.row === 1 && board.getPiece(new Square(square.row + 2, square.col)) === undefined)
                 moves.push(new Square(square.row + 2, square.col))
         }
         else {
+            if(board.getPiece(new Square(square.row - 1, square.col)) !== undefined)
+                return moves;
             moves.push(new Square(square.row - 1, square.col));
-            if(square.row === 6)
+            if(square.row === 6 && board.getPiece(new Square(square.row - 2, square.col)) === undefined)
                 moves.push(new Square(square.row - 2, square.col));
         }
         return moves;
