3839c16f6bb0833e6054c98d54af63a3056126e5
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-13 10:08:11 +0300

    knight can capture enemy pieces, but not king

--- src/engine/board.ts
+++ src/engine/board.ts
@@ -165,8 +165,14 @@ export default class Board {
         const cols: number[] = [1, 2,  2,  1, -1, -2, -2, -1];
         for(let index: number = 0; index <= 7; index++) {
             const newSquare: Square = new Square(square.row + rows[index], square.col + cols[index]);
-            if (this.checkBounds(newSquare))
+            if (this.checkBounds(newSquare)) {
+                if (this.getPiece(newSquare) !== undefined) {
+                    if(this.getPiece(newSquare)?.player !== this.currentPlayer && !(this.getPiece(newSquare) instanceof King))
+                        moves.push(newSquare);
+                    break;
+                }
                 moves.push(newSquare);
+            }
         }
         return moves;
     }
